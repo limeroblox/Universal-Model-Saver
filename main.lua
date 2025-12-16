37facaa703a433d17aced250d7c5128b28213546
Lime <limeroblox2019@gmail.com>
2025-12-16 13:43:49 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,4 @@
 local webhookformat = loadstring(game:HttpGet("https://raw.githubusercontent.com/limeroblox/Universal-Model-Saver/refs/heads/main/Webhook/format.json")) -- Fetches the webhook format for webhook stuff
 loadstring(game:HttpGet("https://raw.githubusercontent.com/limeroblox/Universal-Model-Saver/refs/heads/main/Ui/Main.lua"))() -- Loads the Main UI
+
+local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
